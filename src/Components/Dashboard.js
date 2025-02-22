8485f78eed754b6286448d648efa91ed8f1d558c
Eileen Lidueñez <eileenlc0509@gmail.com>
2025-02-22 14:26:23 -0500

    Commits del Proyecto

---
+++ src/Components/Dashboard.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Route, Routes, Outlet } from 'react-router-dom';
+import Sidebar from './Sidebar';
+import PostsSection from './PostsSection';
+import CommentsSection from './CommentsSection';
+import UsersSection from './UsersSection';
+import RolesSection from './RolesSection';
+import Navbar from './Navbar';
+import CreatePost from './CreatePost';
+import CreateComment from './CreateComment';
+import CreateUser from './CreateUser';
+import '../styles.css';
+
+const Dashboard = () => {
+  return (
+    <div className="dashboard-layout">
+      <Navbar />
+      <div className="dashboard-main flex">
+        <Sidebar /> 
+        <div className="content flex-1 p-8">
+          <h1 className="text-4xl font-bold text-blue-600 mb-8">¡Bienvenido al Dashboard!</h1>
+          
+          <Routes>
+            <Route path="posts" element={<PostsSection />} />
+            <Route path="posts/create" element={<CreatePost />} />
+            <Route path="comments/create" element={<CreateComment />} />
+            <Route path="comments" element={<CommentsSection />} />
+            <Route path="users" element={<UsersSection />} />
+            <Route path="users/create" element={<CreateUser />} />
+            <Route path="roles" element={<RolesSection />} />
+          </Routes>
+
+          <Outlet />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
